58431fa049d0de5b22cc18192d2db773f089a257
Nelsonmwaura <Nelsontoby5@gmail.com>
2020-07-30 21:52:58 +0300

    addiing hover text

--- index.js
+++ index.js
@@ -1,14 +1,17 @@
-
+$('#work').on("click",function(e){
+    $('.span').hover('slow');
+    $('.work1').hover("click");
+   });
 $(document).ready(function () {
     $("#blanks form").submit(function (event) {
         var nameInput = $("input#name").val();
         var emailInput = $("input#email").val();
-        var animalInput = $("input#animal").val();
+        var messageInput = $("input#animal").val();
         $(".name").text(nameInput);
         $(".email").text(emailInput);
         $(".message").text(messageInput);
 
-        $("#story").show();
+        $("#recieved").show();
 
         event.preventDefault();
     });
