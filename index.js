c5c180432e7421cb03ca33f98050887f95b3d740
Nelsonmwaura <52164688+Nelsonmwaura@users.noreply.github.com>
2020-08-05 15:09:35 +0300

    adding js icons hover

--- index.js
+++ index.js
@@ -1,14 +1,62 @@
-$(document).ready(function () {
-    $("#blanks form").submit(function (event) {
-        var nameInput = $("input#name").val();
-        var emailInput = $("input#email").val();
-        var messageInput = $("input#animal").val();
-        $(".name").text(nameInput);
-        $(".email").text(emailInput);
-        $(".message").text(messageInput);
+$(function () {
+    let desp = $('#desp');
+    let devp = $('#devp');
+    let prop = $('#prop');
+    let destext = $('#destxt');
+    let devtext = $('#devtext');
+    let protext = $('#protext');
 
-        $("#recieved").show();
+    $(desp).hide();
+    $(devp).hide();
+    $(prop).hide();
 
-        event.preventDefault();
+    //Show paragraph content
+    $('.des').on('click', function () {
+        $('.des').slideUp('slow');
+        $(desp).show(500);
+        $(destext).animate({ fontWeight: 'bolder', fontSize: '30px' })
+
     });
-});
+    $('.dev').on('click', function () {
+        $('.dev').slideUp('slow');
+        $(devp).show(500);
+        $(devtext).animate({ fontWeight: 'bolder', fontSize: '30px' })
+    });
+    $('.pro').on('click', function () {
+        $('.pro').slideUp('slow');
+        $(prop).show(500);
+        $(protext).animate({ fontWeight: 'bolder', fontSize: '30px' })
+    });
+
+    //Hide paragraph content
+    $(desp, destext).on('click', function () {
+        $('.des').slideDown();
+        $(desp).hide(500);
+        $(destext).animate({ fontWeight: 'normal', fontSize: '16px' })
+    });
+    $(devp, devtext).on('click', function () {
+        $('.dev').slideDown();
+        $(devp).hide(500);
+        $(devtext).animate({ fontWeight: 'normal', fontSize: '16px' })
+    });
+    $(prop, protext).on('click', function () {
+        $('.pro').slideDown();
+        $(prop).hide(500);
+        $(protext).animate({ fontWeight: 'normal', fontSize: '16px' })
+    })
+
+    $("form#form").on('submit', function (event) {
+        event.preventDefault();
+        let name = $("input#name").val();
+        let email = $("input#email").val();
+        let message = $("textarea#message").val();
+
+        if ($("input#name").val() && $("input#email").val()) {
+            alert("Hey " + name + ", Thanks for reaching out, we'll get back to you shortly...cheers");
+        }
+        else {
+            alert("Please provide your correct name and email!");
+        }
+
+    });
+// $(document).ready(function () {
